13b05a3375bc8a8ed3c02531391560ddb9a7ffcc
birukindrias <birukindrias@gmail.com>
2023-02-23 08:30:32 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'login page post'
+    else:
+        return 'login page get'
